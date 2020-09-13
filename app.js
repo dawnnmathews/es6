f135459ae8025b8b71f11df79fbece2d2c2cb0c7
dawn n mathews <dawnmathews11@gmail.com>
2020-09-13 01:55:06 -0400

    added example for template literals

---
+++ app.js
@@ -0,0 +1,11 @@
+// Old way
+
+let firstName = 'peter'
+let lastName = 'smith'
+const fullName = firstName + ' ' + lastName
+console.log(fullName)
+
+// Template Literals
+
+const fullNametl = `${firstName} ${lastName}`
+console.log(fullNametl)
